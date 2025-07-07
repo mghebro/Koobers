fd7ec04d9376f9abae424179b3d286bb878a094d
nikanikonikusha <zery0p@mail.ru>
2025-07-07 14:01:34 +0400

    Navbar Quick Fix

---
+++ src/app/shared/modal-form/modal-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-form',
+  standalone: false,
+  templateUrl: './modal-form.html',
+  styleUrl: './modal-form.scss'
+})
+export class ModalForm {
+
+}
