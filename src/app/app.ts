9361fc064f35a7d0ed64b11dcebe714e98456985
nikanikonikusha <zery0pg@gmail.com>
2025-07-08 04:35:57 -0700

    Quick Fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -20,10 +20,12 @@ export class App {
     // Listen to route changes for animations
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
+
+        document.body.style.overflow = '';
         setTimeout(() => {
           window.scrollTo({ top: 0, behavior: 'smooth' });
         }, 100);
-        
+
 
       }
     });
@@ -32,7 +34,7 @@ export class App {
   isComingSoonPage(): boolean {
     return this.location.path() === '/coming-soon';
   }
-  
+
   prepareRoute(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
   }
